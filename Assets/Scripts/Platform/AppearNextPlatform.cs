936c3937f323e6b6e284e6052cf5f29041b8bf75
Chanmob <sellingout@naver.com>
2019-06-11 00:37:24 +0900

    밟으면 사라지는/ 다음 발판 나타나는 스크립트 생성

---
+++ Assets/Scripts/Platform/AppearNextPlatform.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class AppearNextPlatform : MonoBehaviour
+{
+    public GameObject nextPlatform;
+
+    public float delayDisapper;
+
+    public bool first = false;
+
+    private void OnEnable()
+    {
+        if(!first)
+            StartCoroutine(NextPlatformCoroutine());
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && collision.contacts[0].normal.y <= -0.8f)
+        {
+            if (nextPlatform != null)
+                nextPlatform.SetActive(true);
+        }
+    }
+
+    private IEnumerator NextPlatformCoroutine()
+    {
+        yield return new WaitForSeconds(delayDisapper);
+
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Platform/DisappearPlatform.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class DisappearPlatform : MonoBehaviour
+{
+    public float delayDisappear;
+    public float delayApeear;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && collision.contacts[0].normal.y <= -0.9f)
+        {
+            StartCoroutine(Disappear());
+        }
+    }
+
+    private IEnumerator Disappear()
+    {
+        yield return new WaitForSeconds(delayDisappear);
+
+        Invoke("Appear", delayApeear);
+        this.gameObject.SetActive(false);
+    }
+
+    private void Appear()
+    {
+        this.gameObject.SetActive(true);
+    }
+}
