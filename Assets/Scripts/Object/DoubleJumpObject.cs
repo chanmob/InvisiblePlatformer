e032aec236d97a29d990a345b00b9d4612290e70
Chanmob <sellingout@naver.com>
2019-06-11 16:19:51 +0900

    더블점프 할 수 있게 해주는 스크립트 생성

---
+++ Assets/Scripts/Object/DoubleJumpObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class DoubleJumpObject : MonoBehaviour
+{
+    public float delayVisible;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.GetComponent<Player>().grounded = true;
+            Invoke("VisibleObject", delayVisible);
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void VisibleObject()
+    {
+        gameObject.SetActive(true);
+    }
+}
