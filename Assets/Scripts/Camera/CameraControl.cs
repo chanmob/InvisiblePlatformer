30e5b0176cad535812ba37b197cecac8890911f9
Chanmob <sellingout@naver.com>
2019-06-19 23:23:28 +0900

    레벨 10까지 작업, 맵마다 카메라 컨트롤 박스 콜라이더 추가해야함

--- Assets/Scripts/Camera/CameraControl.cs
+++ Assets/Scripts/Camera/CameraControl.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     public BoxCollider2D mapBox2d;
-    public BoxCollider2D cameraBox2d;
-
+  
     private GameObject player;
     public GameObject objectCam;
 
@@ -14,27 +13,39 @@ public class CameraControl : MonoBehaviour
 
     private float cameraHalfSize;
 
-    // Start is called before the first frame update
     void Start()
-    {        
-        cameraHalfSize = cameraBox2d.size.y * 0.5f;
+    {
+        mapBox2d = GetComponent<BoxCollider2D>();
+        cameraHalfSize = mapBox2d.size.y;
         player = GameObject.FindGameObjectWithTag("Player");
         pos = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(mapBox2d.bounds.max.y < player.transform.position.y)
         {
-            this.transform.position = new Vector3(pos.x, pos.y + cameraHalfSize, pos.z);
-            objectCam.transform.position = new Vector3(pos.x, pos.y + cameraHalfSize, pos.z);
+            this.transform.position = new Vector3(pos.x, pos.y + mapBox2d.size.y, pos.z);
+            objectCam.transform.position = new Vector3(pos.x, pos.y + mapBox2d.size.y, pos.z);
             pos = transform.position;
         }
         else if (mapBox2d.bounds.min.y > player.transform.position.y)
         {
-            this.transform.position = new Vector3(pos.x, pos.y - cameraHalfSize, pos.z);
-            objectCam.transform.position = new Vector3(pos.x, pos.y - cameraHalfSize, pos.z);
+            this.transform.position = new Vector3(pos.x, pos.y - mapBox2d.size.y, pos.z);
+            objectCam.transform.position = new Vector3(pos.x, pos.y - mapBox2d.size.y, pos.z);
+            pos = transform.position;
+        }
+
+        if (mapBox2d.bounds.max.x < player.transform.position.x)
+        {
+            this.transform.position = new Vector3(pos.x + mapBox2d.size.x, pos.y, pos.z);
+            objectCam.transform.position = new Vector3(pos.x + mapBox2d.size.x, pos.y, pos.z);
+            pos = transform.position;
+        }
+        else if (mapBox2d.bounds.min.x > player.transform.position.x)
+        {
+            this.transform.position = new Vector3(pos.x - mapBox2d.size.x, pos.y, pos.z);
+            objectCam.transform.position = new Vector3(pos.x - mapBox2d.size.x, pos.y, pos.z);
             pos = transform.position;
         }
     }
