7af9f1f2718249c2c41249a3a65aa7a52402910d
Chanmob <sellingout@naver.com>
2019-06-20 00:29:49 +0900

    닿으면 일정시간 후 없어지는 벽 추가, 스테이지 13까지 어느정도 작업

---
+++ Assets/Scripts/Platform/EnterActivePlatform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class EnterActivePlatform : MonoBehaviour
+{
+    public float disappearTime;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Invoke("Disappear", disappearTime);
+        }
+    }
+
+    private void Disappear()
+    {
+        gameObject.SetActive(false);
+    }
+}
