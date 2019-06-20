9fc39ab0a4af7da3d9a927902e156734796fcafb
Chanmob <sellingout@naver.com>
2019-06-20 18:55:04 +0900

    경로 따라 이동하는 발판 추가

---
+++ Assets/Scripts/Platform/PathMoveManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PathMoveManager : MonoBehaviour
+{
+    public List<PathMovePlatform> pathPlatformLists = new List<PathMovePlatform>();
+
+    public List<GameObject> pathLists = new List<GameObject>();
+
+    private bool started = false;
+
+    private void Start()
+    {
+        pathLists = GameObject.FindGameObjectsWithTag("Path").ToList();
+        pathPlatformLists = GetComponentsInChildren<PathMovePlatform>().ToList();
+
+        for(int i = 0; i < pathPlatformLists.Count; i++)
+        {
+            pathPlatformLists[i].wayPoints = pathLists;
+        }
+    }
+
+    public void StartMove()
+    {
+        if (started)
+            return;
+
+        started = true;
+        for(int i = 0; i < pathPlatformLists.Count; i++)
+        {
+            pathPlatformLists[i].move = true;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+
+        for (int i = 1; i < pathLists.Count; i++)
+        {
+            Gizmos.DrawLine(pathLists[i - 1].transform.position, pathLists[i].transform.position);
+        }
+    }
+}
---
+++ Assets/Scripts/Platform/PathMovePlatform.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class PathMovePlatform : MonoBehaviour
+{
+    public List<GameObject> wayPoints = new List<GameObject>();
+
+    private Vector2 direction = Vector2.zero;
+
+    private int wayPointIndex = 0;
+
+    public float speed;
+
+    public bool move;
+
+    private void FixedUpdate()
+    {
+        if (!move)
+            return;
+
+        direction = wayPoints[wayPointIndex].transform.position - transform.position;
+
+        transform.Translate(direction.normalized * speed * Time.deltaTime);
+
+        if(Vector2.Distance(transform.position, wayPoints[wayPointIndex].transform.position) <= 0.05f)
+        {
+            transform.position = wayPoints[wayPointIndex].transform.position;
+            wayPointIndex++;
+
+            if(wayPointIndex >= wayPoints.Count)
+            {
+                move = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            var manager = GetComponentInParent<PathMoveManager>();
+
+            manager.StartMove();
+        }
+    }
+}
