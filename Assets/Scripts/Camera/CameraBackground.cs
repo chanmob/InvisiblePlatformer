25bad90d43b9a4a4e2517bf9841fbac4785e1ab8
Chanmob <sellingout@naver.com>
2019-07-03 15:15:07 +0900

    스테이지 선택 창 슬라이드 뷰 / 카메라 배경 스크립트 추가

---
+++ Assets/Scripts/Camera/CameraBackground.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBackground : MonoBehaviour
+{
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+        StartCoroutine(CameraBackgroundControl());
+    }
+
+    private IEnumerator CameraBackgroundControl()
+    {
+        Color32[] colors = new Color32[]
+        {
+            new Color32(20, 0, 0, 255),
+            new Color32(20, 0, 20, 255),
+            new Color32(0, 0, 20, 255),
+            new Color32(0, 20, 20, 255),
+            new Color32(0, 20, 0, 255),
+            new Color32(20, 20, 0, 255),
+            new Color32(0, 0, 0, 255)
+        };
+
+        Color32 color;
+        int idx = 0;
+
+        while (true)
+        {
+            color = colors[idx];
+            cam.backgroundColor = Color.Lerp(cam.backgroundColor, color, 2.5f * Time.deltaTime);
+
+            if (cam.backgroundColor == color)
+            {
+                idx++;
+
+                if (idx >= colors.Length)
+                    idx = 0;
+            }
+
+            yield return null;
+        }
+    }
+}
