65a87760c77bc5c2835a5ea4ea77d027c144830a
Chanmob <sellingout@naver.com>
2019-07-23 22:01:29 +0900

    수정

---
+++ Assets/Scripts/Player/PlayerStartPoint.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStartPoint : MonoBehaviour
+{
+    void Start()
+    {
+        GameObject.FindGameObjectWithTag("Player").transform.position = this.transform.position;
+    }
+}
